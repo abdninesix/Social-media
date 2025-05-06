c824b6e4157a6588d9b59df298baca8abc513a98
abdninesix <mohabd1995@gmail.com>
2025-05-06 18:15:12 +0500

    New

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { IKVideo } from "imagekitio-next";
 
 const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
@@ -15,7 +16,7 @@ const Video = ({ path, className }: VideoTypes) => {
       path={path}
       className={className}
       transformation={[
-        { width: "1920", height: "1080", q: "90" },
+        { width: "1920", height: "1080", quality: 90 },
         { raw: "l-text,i-LamaDev,fs-100,co-white,l-end" },
       ]}
       controls
