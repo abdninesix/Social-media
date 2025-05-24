8cf3be38d09e44d9316195477014ded1feb0b2fa
abdninesix <mohabd1995@gmail.com>
2025-05-24 22:49:11 +0500

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ const Homepage = () => {
         <Link className="pb-3 flex items-center" href="/">Following</Link>
         <Link className="hidden pb-3 md:flex items-center" href="/">Next.js</Link>
         <Link className="hidden pb-3 md:flex items-center" href="/">Python</Link>
-        <Link className="hidden pb-3 md:flex items-center" href="/">CSS</Link>
+        <Link className="hidden pb-3 md:flex items-center" href="/">Tailwind v4</Link>
       </div>
       <Share />
       <Feed />
