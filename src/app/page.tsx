5f725f1c83b4a0b56c973b1c40605aad0c0bf985
abdninesix <mohabd1995@gmail.com>
2025-05-26 22:23:40 +0500

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,10 @@ const Homepage = () => {
       <div className='px-4 pt-4 flex justify-between text-textGray font-bold border-b-[1px] border-borderGray'>
         <Link className="pb-3 flex items-center border-b-4 border-iconBlue" href="/">For you</Link>
         <Link className="pb-3 flex items-center" href="/">Following</Link>
-        <Link className="hidden pb-3 md:flex items-center" href="/">Recent</Link>
-        <Link className="hidden pb-3 md:flex items-center" href="/">Trending</Link>
-        <Link className="hidden pb-3 md:flex items-center" href="/">Popular</Link>
-        <Link className="hidden pb-3 md:flex items-center" href="/">Oldest</Link>
+        <Link className="hidden pb-3 md:flex items-center" href="/">Next.js</Link>
+        <Link className="hidden pb-3 md:flex items-center" href="/">React.js</Link>
+        <Link className="hidden pb-3 md:flex items-center" href="/">Tailwind</Link>
+        <Link className="hidden pb-3 md:flex items-center" href="/">MongoDB</Link>
       </div>
       <Share />
       <Feed />
